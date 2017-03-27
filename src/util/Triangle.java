fb907458ac9b677b6bbbf9615e2fe03c3774141c
nico59600 <nicolasgeris@gmail.com>
2017-03-27 09:06:16 +0200

    Javadoc classe Triangle

--- src/util/Triangle.java
+++ src/util/Triangle.java
@@ -5,25 +5,79 @@ import com.jogamp.opengl.GL2;
 
 import model.Oiseau;
 
-
+/**
+ * Cette classe représente un triangle à afficher dans un espace en 3
+ * dimensions. L'objet Triangle est caractérisé par 3 Point3D
+ * 
+ * @author Nico
+ *
+ */
 public class Triangle {
-	
+
+	/**
+	 * premier point
+	 */
 	private Point3D p1;
+
+	/**
+	 * deuxième point
+	 */
 	private Point3D p2;
+
+	/**
+	 * troisième point
+	 */
 	private Point3D p3;
 
+	/**
+	 * Constructeur prenant 9 coordonnées en argument
+	 * 
+	 * @param ax
+	 *            Point3D p1 : x
+	 * @param ay
+	 *            Point3D p1 : y
+	 * @param az
+	 *            Point3D p1 : z
+	 * @param bx
+	 *            Point3D p2 : x
+	 * @param by
+	 *            Point3D p2 : y
+	 * @param bz
+	 *            Point3D p2 : z
+	 * @param cx
+	 *            Point3D p3 : x
+	 * @param cy
+	 *            Point3D p3 : y
+	 * @param cz
+	 *            Point3D p3 : z
+	 */
 	public Triangle(float ax, float ay, float az, float bx, float by, float bz, float cx, float cy, float cz) {
 		p1 = new Point3D(ax, ay, az);
 		p2 = new Point3D(bx, by, bz);
 		p3 = new Point3D(cx, cy, cz);
 	}
 
+	/**
+	 * Constructeur prenant 3 Point3D en argument
+	 * 
+	 * @param p1
+	 *            Point3D 1
+	 * @param p2
+	 *            Point3D 2
+	 * @param p3
+	 *            Point3D 3
+	 */
 	public Triangle(Point3D p1, Point3D p2, Point3D p3) {
 		this.p1 = p1;
 		this.p2 = p2;
 		this.p3 = p3;
 	}
 
+	/**
+	 * Méthode permettant d'afficher l'objet Triangle dans l'espace
+	 * 
+	 * @param gl
+	 */
 	public void render(GL2 gl) {
 		gl.glBegin(GL.GL_TRIANGLES);
 		gl.glVertex3f(p1.getX(), p1.getY(), p1.getZ());
@@ -32,6 +86,13 @@ public class Triangle {
 		gl.glEnd();
 	}
 
+	/**
+	 * Méthode permettant d'afficher l'objet Triangle dans l'espace selon les
+	 * coordonnées de l'oiseau
+	 * 
+	 * @param gl
+	 * @param o
+	 */
 	public void render(GL2 gl, Oiseau o) {
 		gl.glBegin(GL.GL_TRIANGLES);
 		gl.glVertex3f(p1.getX() + o.getX(), p1.getY() + o.getY(), p1.getZ() + o.getZ());
@@ -41,7 +102,15 @@ public class Triangle {
 	}
 
 	/**
-	 * @deprecated
+	 * Méthode permettant de déplacer le triangle. Effectue une translation sur
+	 * les 3 axes
+	 * 
+	 * @param x
+	 *            translation sur l'axe x
+	 * @param y
+	 *            translation sur l'axe y
+	 * @param z
+	 *            translation sur l'axe z
 	 */
 	public void move(float x, float y, float z) {
 		p1.setPos(x, y, z);
@@ -54,6 +123,10 @@ public class Triangle {
 	}
 
 	@Override
+	/**
+	 * Méthode toString de classe affichant les coordonnées de chaque Point3D du
+	 * triangle
+	 */
 	public String toString() {
 		return "Triangle [p1=" + p1.getX() + ", p2=" + p1.getY() + ", p3=" + p1.getZ() + "]";
 	}
